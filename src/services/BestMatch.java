373072d332a237c46aef65090d5cc6e72c9d9255
Tejesh Raut <tejesh22.raut@gmail.com>
2016-06-28 19:19:11 +0530

    Changed the algo for matching strings

--- src/services/BestMatch.java
+++ src/services/BestMatch.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author Tejesh_Raut
@@ -14,51 +15,76 @@ public class BestMatch
 {
 	public static ArrayList<String> BestQuery(String q , String[] NonKeywords, Map<String, String> query) throws Exception
 	{
-		String s, syn;
+		String s, s2;
+		s2="";
 		ArrayList<String> s1 = new ArrayList<String>();
-		String[] ss,qs, syns;
 		String[] allwords = KeepKeywords.SeparateWords(q);
 		String Keywords = KeepKeywords.Keywords(allwords, NonKeywords);
-		qs = Keywords.split("\\s+");// words from the query string
-		int c=0;
-		int cmax = 0;
-		for (Map.Entry<String,String> entry : query.entrySet()) 
+		int d, dmin;
+		d = 1000;
+		dmin = 1000;
+		for (Map.Entry<String,String> entry : query.entrySet())// loop through each query in the database
 		{
-			c=0;
+			d = 1000;
 			s = entry.getKey();// a query from map(database)
-			ss = s.split("\\s+");//array of words of the query from map(database)
-			for (int i=0; i<qs.length; i++)
+			//Try all ways of asking q
+			ArrayList<String> qways = new ArrayList<String>();
+			qways = getAllWays(Keywords);
+			for(int i=0; i<qways.size(); i++)
 			{
-				for (int j=0; j<ss.length; j++)
+				if(d > StringUtils.getLevenshteinDistance(q, qways.get(i)))
 				{
-					syn = synonym(ss[j]);
-					syns = syn.split("\\s+");//array of words of synonyms
-					for(int k=0; k<syns.length;k++)
-					{
-						if(qs[i].equalsIgnoreCase(syns[k]))
-						{
-							c++;
-						}
-					}
+					d = StringUtils.getLevenshteinDistance(q, qways.get(i));
 				}
 			}
-			if(cmax == c)
+			if(dmin > d)
 			{
-				s1.add(s);
-			}
-			if(cmax<c)
-			{
-				cmax = c;
-				s1 = new ArrayList<String>();
-				s1.add(s);
+				s2 = s;
+				dmin = d;
 			}
 		}
-		if(cmax == 0)
-		{
-			s1 = new ArrayList<String>();
-			s1.add("Sorry, Please ask some other query");
-		}
+		s1.add(s2);
 		return s1;
+	}
+	
+	public static ArrayList<String> getAllWays (String q) throws FileNotFoundException
+	{
+		ArrayList<String> qways = new ArrayList<String>();
+		String[] qs, syns;
+		String syn;
+		qs = q.split("\\s+");
+		if(qs.length == 1)
+		{
+			syn = synonym(qs[0]);
+			syns = syn.split("\\s+");
+			for(int i=0; i<syns.length;i++)
+			{
+				qways.add(syns[i]);
+			}
+			return(qways);
+		}
+		else
+		{
+			syn = synonym(qs[0]);
+			syns = syn.split("\\s+");
+			String q1;
+			q1="";
+			for(int i=1; i<qs.length; i++)
+			{
+				q1 = q1 + " " + qs[i];
+			}
+			//q1 = string formed after removing first word
+			for(int i=0; i<syns.length;i++)
+			{
+				ArrayList<String> qways1 = new ArrayList<String>();
+				qways1 = getAllWays(q1);
+				for(int j=0; j<qways1.size(); j++)
+				{
+					qways.add(qs[0] + " "+ qways1.get(j));
+				}
+			}
+			return(qways);
+		}
 	}
 	
 	public static String synonym(String s) throws FileNotFoundException
